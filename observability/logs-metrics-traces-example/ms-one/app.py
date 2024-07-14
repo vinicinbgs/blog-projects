2cae109d45864a291f818834a4e8f0d771d21a0b
Vinicius Morais Dutra <viniciusmd@ciandt.com>
2024-07-14 15:33:08 -0300

    wip

--- observability/logs-metrics-traces-example/ms-one/app.py
+++ observability/logs-metrics-traces-example/ms-one/app.py
@@ -1,3 +1,4 @@
+import os
 import json
 import sys
 import requests
@@ -22,12 +23,13 @@ def healthcheck():
         "message": f"Hello from {MS_NAME}!"
     }
     
-    Logger().info('app.py', {
+    Logger().info('Response',  {
         "response": res, 
         "status": "200", 
         "method": "GET", 
         "path": request.path, 
-        "app": MS_NAME
+        "app": MS_NAME,
+        "file": "app.py"
     })
     
     return res
@@ -36,20 +38,47 @@ def do_something():
     numbers = (1, 2, 3, 4, 5)
     total = sum(numbers, 20)
     
-    Logger().info('app.py', {"total": total, "numbers": numbers})
+    Logger().info('do_something', {
+        "total": total, 
+        "numbers": numbers,
+        "app": MS_NAME,
+        "file": "app.py"
+    })
+    
+    filename = 'example.txt'
+    content = 'Hello, this is a sample content to be written to the file.'
+    
+    try:
+        with open(filename, 'w') as file:
+            file.write(content)
+        print(f"Content written to {filename} successfully.")
+    except Exception as e:
+        print(f"An error occurred while writing to the file: {e}")
 
 @app.route('/call-ms-two')
 def call_ms_two():
-    ms_two_endpoint = 'http://ms-two:9999/'
+    ms_two_endpoint = os.getenv('MS_TWO_ENDPOINT', 'http://ms-two:9999')
     
-    Logger().info('app.py', {"method": "GET", "path": "/call-ms-two", "app": MS_NAME})
+    Logger().info('Request', {
+        "method": "GET", 
+        "path": "/call-ms-two", 
+        "app": MS_NAME, 
+        "file": "app.py"
+    })
     
     response = requests.get(ms_two_endpoint, headers={'trace_id': TraceId().get_id()})
     
-    Logger().info('app.py', {"response": response.text, "status": 200, "method": "GET", "path": ms_two_endpoint, "app": MS_NAME})
+    Logger().info('Response', {
+        "response": response.text, 
+        "status": 200, 
+        "method": "GET", 
+        "path": ms_two_endpoint, 
+        "app": MS_NAME, 
+        "file": "app.py"
+    })
     
     return response.text
 
 if __name__ == '__main__':
-    start_http_server(8000)
-    app.run(host='0.0.0.0', port=9999)
+    start_http_server(8000) # Start Prometheus metrics server
+    app.run(host='0.0.0.0', port=9999) # Start Flask app
--- observability/logs-metrics-traces-example/request.sh
+++ observability/logs-metrics-traces-example/request.sh
@@ -1,10 +1,30 @@
 #!bin/sh
 
-random_sleep() {
-    sleep $(( ( RANDOM % 5 )  + 1 ))
-}
+TIMES=1000
+SLEEP=0
+ROUTE="/call-ms-two"
 
-for i in {1..100}; do
-    curl -X GET http://localhost:9998/call-ms-two
-    sleep 5
+for i in "$@"
+do
+case $i in
+    sleep=*)
+    SLEEP="${i#*=}"
+    shift
+    ;;
+    times=*)
+    TIMES="${i#*=}"
+    shift
+    ;;
+    route=*)
+    ROUTE="${i#*=}"
+    shift
+    ;;
+    *)
+    ;;
+esac
+done
+
+for i in $(eval echo {1..$TIMES}); do
+    curl -X GET http://localhost:9998$ROUTE
+    sleep $SLEEP
 done
