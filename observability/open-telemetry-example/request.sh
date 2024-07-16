880f651021e9009bbdbc0e4a4e01bf9353dfeab4
Vinicius Morais Dutra <viniciusmd@ciandt.com>
2024-07-15 22:51:01 -0300

    wip

---
+++ observability/open-telemetry-example/request.sh
@@ -0,0 +1,30 @@
+#!bin/sh
+
+TIMES=1000
+SLEEP=0
+ROUTE="/"
+
+for i in "$@"
+do
+case $i in
+    sleep=*)
+    SLEEP="${i#*=}"
+    shift
+    ;;
+    times=*)
+    TIMES="${i#*=}"
+    shift
+    ;;
+    route=*)
+    ROUTE="${i#*=}"
+    shift
+    ;;
+    *)
+    ;;
+esac
+done
+
+for i in $(eval echo {1..$TIMES}); do
+    curl -X GET http://localhost:9998$ROUTE
+    sleep $SLEEP
+done
